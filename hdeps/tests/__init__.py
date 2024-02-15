84344767d4b8882963da5fe445e43b98a225d0a1
Tim Hatch <tim@timhatch.com>
2024-02-15 08:16:54 -0800

    Increase test coverage by testing the CLI
    Fixes #4

--- hdeps/tests/__init__.py
+++ hdeps/tests/__init__.py
@@ -1,7 +1,14 @@
 from .cache import SimpleCacheTest
 from .markers import EnvironmentMarkersTest
+from .requirements import RequirementsTest
+from .resolution import ResolutionTest
+
+# from .session import LiveSessionTest
 
 __all__ = [
     "EnvironmentMarkersTest",
     "SimpleCacheTest",
+    "RequirementsTest",
+    "ResolutionTest",
+    # "LiveSessionTest",
 ]
