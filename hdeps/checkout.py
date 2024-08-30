93164ba9f24c0f4a0c727379ff43573138327758
Tim Hatch <tim@timhatch.com>
2024-08-29 18:16:03 -0700

    Simplify checkout handling to current metadata-please pr

--- hdeps/checkout.py
+++ hdeps/checkout.py
@@ -1,39 +1,16 @@
-import os.path
 from pathlib import Path
 
 from typing import List, Tuple
 
-from metadata_please import (
-    basic_metadata_from_pep621_checkout,
-    basic_metadata_from_setuptools_checkout,
-)
+from metadata_please import basic_metadata_from_source_checkout
 
 from packaging.requirements import Requirement
 
 
 def read_checkout_reqs(path: Path) -> Tuple[str, List[Requirement]]:
-    try:
-        bm = basic_metadata_from_pep621_checkout(path)
-    except OSError:
-        pass
-    else:
-        if bm.reqs:
-            return (
-                os.path.join(path, "pyproject.toml"),
-                [Requirement(req) for req in bm.reqs],
-            )
-
-    try:
-        bm = basic_metadata_from_setuptools_checkout(path)
-    except OSError:
-        pass
-    else:
-        if bm.reqs:
-            return (
-                os.path.join(path, "setup.cfg"),
-                [Requirement(req) for req in bm.reqs],
-            )
-
-    raise ValueError(
-        f"Path {path} does not contain a currently-supported static metadata.  Try specifying -r requirements.txt"
-    )
+    bm = basic_metadata_from_source_checkout(path)
+    if not bm.reqs:
+        raise ValueError(
+            f"Path {path} did not yield any dependencies, and may not not contain a currently-supported static metadata format (or maybe it just has no deps).  Try specifying -r requirements.txt or omitting."
+        )
+    return (str(path), [Requirement(r) for r in bm.reqs])
